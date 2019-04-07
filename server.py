009b3bcac5b62f753974814f93018c086f9311ea
josephsintum <joseph24jay@gmail.com>
2019-04-07 11:25:26 -0700

    camera resolution updates

--- server.py
+++ server.py
@@ -1,26 +1,28 @@
 import io
-import picamera
 import logging
 import socketserver
 from threading import Condition
 from http import server
+from picamera import PiCamera
 
 # https://picamera.readthedocs.io/en/latest/recipes2.html#web-streaming
 # web server to stream images on request
 
 # Web page
-PAGE="""\
+PAGE = """\
 <html>
 <head>
 <title>picamera MJPEG streaming demo</title>
 </head>
 <body>
 <h1>PiCamera MJPEG Streaming Demo</h1>
-<img src="stream.mjpg" width="1042" height="576" />
+<img src="stream.mjpg" width="820" height="616" />
 </body>
 </html>
 """
 # buffer content
+
+
 class StreamingOutput(object):
     def __init__(self):
         self.frame = None
@@ -37,6 +39,7 @@ class StreamingOutput(object):
                 self.condition.notify_all()
             self.buffer.seek(0)
         return self.buffer.write(buf)
+
 
 class StreamingHandler(server.BaseHTTPRequestHandler):
     def do_GET(self):
@@ -56,7 +59,8 @@ class StreamingHandler(server.BaseHTTPRequestHandler):
             self.send_header('Age', 0)
             self.send_header('Cache-Control', 'no-cache, private')
             self.send_header('Pragma', 'no-cache')
-            self.send_header('Content-Type', 'multipart/x-mixed-replace; boundary=FRAME')
+            self.send_header(
+                'Content-Type', 'multipart/x-mixed-replace; boundary=FRAME')
             self.end_headers()
             try:
                 while True:
@@ -77,11 +81,28 @@ class StreamingHandler(server.BaseHTTPRequestHandler):
             self.send_error(404)
             self.end_headers()
 
+
 class StreamingServer(socketserver.ThreadingMixIn, server.HTTPServer):
     allow_reuse_address = True
     daemon_threads = True
 
-with picamera.PiCamera(resolution='1024x576', framerate=24) as camera:
+
+with PiCamera() as camera:
+    # Forced sensor mode, 1640x1232, full FoV. See:
+    # https://picamera.readthedocs.io/en/release-1.13/fov.html#sensor-modes
+    # This is the resolution inference run on.
+    camera.sensor_mode = 4
+
+    # Scaled and cropped resolution. If different from sensor mode implied
+    # resolution, inference results must be adjusted accordingly. This is
+    # true in particular when camera.start_recording is used to record an
+    # encoded h264 video stream as the Pi encoder can't encode all native
+    # sensor resolutions, or a standard one like 1080p may be desired.
+    camera.resolution = (1640, 1232)
+
+    # Start the camera stream.
+    camera.framerate = 24
+
     output = StreamingOutput()
     camera.start_recording(output, format='mjpeg')
     try:
